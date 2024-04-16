69135562ebc00d758520f24217abf9b664104cee
galgrayzer <galgrayzer@gmail.com>
2024-04-17 00:46:32 +0300

    add assembly32 compiling

---
+++ compileAsm32.py
@@ -0,0 +1,22 @@
+from os import remove, system
+from sys import argv
+
+
+def main():
+    if len(argv) < 2:
+        print("Usage: python compileAsm32.py <file.asm>")
+        return
+
+    filename = argv[1]
+    if filename[-4:] != ".asm":
+        print("Invalid file extension")
+        return
+
+    filename = filename[:-4]
+    system(f"nasm -f elf {filename}.asm -o {filename}.o")
+    system(f"ld -m elf_i386 {filename}.o -o {filename}")
+    remove(f"{filename}.o")
+
+
+if __name__ == '__main__':
+    main()
